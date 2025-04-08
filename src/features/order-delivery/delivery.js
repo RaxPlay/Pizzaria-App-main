cf4fc2645784bd08f8f3796f5e1199f6dc8d84bf
RaxPlay <121737678+RaxPlay@users.noreply.github.com>
2025-04-07 22:52:49 -0700

    Bugs and Delivery Site Solved some minor UI bugs and almost finished with delivery site.

--- src/features/order-delivery/delivery.js
+++ src/features/order-delivery/delivery.js
@@ -1,21 +1,27 @@
-//exportation
-export {address};
-
 //Variables
 const getDataBtn = document.querySelector('.get-data-btn');
 const street = document.querySelector('.street');
 const city = document.querySelector('.city');
-const state = document.querySelector('#state-select');
 const zipCode = document.querySelector('.zip-code');
 const country = document.querySelector('#country-select');
+const state = document.querySelector('#state-select');
+const deliveryTimeDiv = document.querySelector('#delivery-time')
 
 getDataBtn.addEventListener('click', function () {
-    //address object
     let address = {
         street: street.value,
         city: city.value,
         state: state.value,
         zipCode: zipCode.value,
         country: country.value,
-    };
+    }; 
+
+    deliveryTimeDiv.classList.add('delivery-time-transition');
+    deliveryTimeDiv.style.display = `flex`;
+
+
+    console.log(address);
+    console.log(deliveryTimeDiv);
+
+    return false;
 });
