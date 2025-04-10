cfa6354d6143713857f49fa05141a88efcf19815
RaxPlay <121737678+RaxPlay@users.noreply.github.com>
2025-04-09 23:08:37 -0700

    Start of Menu layout This is the start of the menu and creation of pizza for user.

---
+++ src/database/menu.js
@@ -0,0 +1,15 @@
+export {menu};
+
+const menu = [
+    //Pizzas
+    "pepperoni",
+    "neapolitan",
+    "hawaiian",
+    "portuguese",
+    "four-cheese",
+    "margherita",
+    "barbecue",
+    "vegetarian",
+    "seafood",
+    "fugazzeta",
+]
---
+++ src/features/menu/show-menu.js
@@ -0,0 +1,5 @@
+//Modules
+import { menu } from "../../database/menu";
+
+//Variables
+let createOwnPizzaBtn = document.querySelector("create-own-pizza-btn");
