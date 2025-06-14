42a510ce3aae74a9498d0b3850e69b6c3e1af154
RaxPlay <121737678+RaxPlay@users.noreply.github.com>
2025-06-13 18:18:30 -0700

    Fixing and Cleaning fixing little visual bugs, and cleaning some unneccesary code

--- src/features/menu/show-menu.js
+++ src/features/menu/show-menu.js
@@ -1,5 +1,14 @@
 //Modules
-import { menu } from "../../database/menu";
-
+export default id
 //Variables
-let createOwnPizzaBtn = document.querySelector("create-own-pizza-btn");
+
+let createOwnPizzaBtn = document.querySelector(".create-own-pizza-btn");
+let cartButton = document.querySelector('.cart')
+
+//functions
+
+function addToCart(id){
+    console.log(id)
+
+    alert("Item has been added succesfully");
+}
